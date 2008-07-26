3c61d0976bcb7ea2a8425bcbbd7cb2b08c30caa4
Phanx <addons@phanx.net>
2008-07-26 08:06:55 +0000

    - Add full support for localization - Add sample localization file - Add README.TXT file - Tons of cleanup - Drycoded, needs testing!

--- Core.lua
+++ Core.lua
@@ -2,85 +2,84 @@
 	Diplomancer: automatic location-based faction watch
 	by Phanx < addons AT phanx net >
 	Based upon Smart Faction Watch by Charnow
-
-	DO NOT include this addon in compilations or otherwise
-	redistribute it without my express prior consent.
-
-	Feel free to distribute modified versions of this addon in
-	any way you wish, AFTER you have changed the name of the
-	addon to something that does not include the name of this
-	addon, or my name.
+	See included README.TXT for license and additional information.
 ----------------------------------------------------------------------]]
 
 Diplomancer = CreateFrame("Frame")
 Diplomancer.version = tonumber(GetAddOnMetadata("Diplomancer", "Version"))
+Diplomancer:SetScript("OnEvent", function(self, event, ...) if self[event] then return self[event](self, ...)	end end)
+Diplomancer:RegisterEvent("PLAYER_ENTERING_WORLD")
 
 local Diplomancer = Diplomancer
-local db, zones, subzones, races, inactive, initialized, racialFaction, onTaxi
+local db, zones, subzones, racial, onTaxi
 
-local function print(text)
+local L = setmetatable(DiplomancerLocals or {}, { __index = function(t, k) rawset(t, k, k) return k end })
+if DiplomancerLocals then DiplomancerLocals = nil end
+
+local function Print(text)
 	ChatFrame1:AddMessage("|cff33ff99Diplomancer:|r "..text)
 end
 
-local function debug(text)
+local function Debug(text)
 	ChatFrame1:AddMessage("|cffff3399[DEBUG] Diplomancer:|r "..text)
 end
-
-Diplomancer:SetScript("OnEvent", function(self, event, ...)
-	if self[event] then
-		self[event](self, ...)
-	else
-		self:UnregisterEvent(event)
-		error("no handler found for event "..event)
-	end
-end)
-
-Diplomancer:RegisterEvent("PLAYER_LOGIN")
 
 --[[------------------------------------------------------------
 	Initialize
 --------------------------------------------------------------]]
-function Diplomancer:PLAYER_LOGIN()
-	zones, subzones, races = self:GetData()
+function Diplomancer:PLAYER_ENTERING_WORLD()
+	zones, subzones, racial = self:GetData()
+	racial = racial[UnitRace("player")]
 
-	racialFaction = races[UnitRace("player")]
-
+	local defaults = {
+		default = nil,
+		verbose = nil,
+		version = self.version,
+	}
 	if not DiplomancerSettings then
-		DiplomancerSettings = {
-			default = nil,
-			verbose = nil,
-			version = self.version,
-		}
+		DiplomancerSettings = defaults
+	elseif DiplomancerSettings.version ~= self.version then
+		local temp = defaults
+		for k, v in pairs(DiplomancerSettings) do
+			if defaults[k] then
+				temp[k] = v
+			end
+		end
+		temp.version = self.version
+		DiplomancerSettings = temp
 	end
 	db = DiplomancerSettings
-
-	self:RegisterEvent("ZONE_CHANGED")
-	self:RegisterEvent("ZONE_CHANGED_NEW_AREA")
-	self:RegisterEvent("PLAYER_CONTROL_LOST")
-	self:RegisterEvent("PLAYER_CONTROL_GAINED")
 
 	if UnitOnTaxi("player") then
 		onTaxi = true
 	else
 		self:Update()
 	end
+
+	self:RegisterEvent("ZONE_CHANGED")
+	self:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+	self:RegisterEvent("PLAYER_CONTROL_LOST")
+	self:RegisterEvent("PLAYER_CONTROL_GAINED")
+
+	self:UnregisterEvent("PLAYER_ENTERING_WORLD")
+	self.PLAYER_ENTERING_WORLD = nil
 end
 
 --[[------------------------------------------------------------
 	Check taxi status
 --------------------------------------------------------------]]
 function Diplomancer:PLAYER_CONTROL_LOST()
-	-- debug("lost control")
+	-- Debug("Control lost")
 	if UnitOnTaxi("player") then
-		-- debug("starting taxi ride")
+		-- Debug("Taxi ride started")
 		onTaxi = true
 	end
 end
 
 function Diplomancer:PLAYER_CONTROL_GAINED()
-	-- debug("gained control")
+	-- Debug("Control gained")
 	if onTaxi then
-		-- debug("ending taxi ride")
+		-- Debug("Taxi ride ended")
 		onTaxi = false
 		self:Update()
 	end
@@ -90,42 +89,42 @@ end
 	Update watched faction for the current zone
 --------------------------------------------------------------]]
 function Diplomancer:ZONE_CHANGED()
-	-- debug("changed subzone")
+	-- Debug("Subzone changed")
 	self:Update()
 end
 
 function Diplomancer:ZONE_CHANGED_NEW_AREA()
-	-- debug("changed zone")
+	-- Debug("Zone changed")
 	self:Update()
 end
 
 function Diplomancer:Update()
 	if onTaxi then
-		-- debug("skipping update while on taxi")
+		-- Debug("On taxi; skipping update")
 		return
 	end
 
 	local zone, subzone = GetRealZoneText(), GetSubZoneText()
-	-- debug("zone = "..zone.."; subzone = "..subzone)
+	-- Debug("zone = "..zone.."; subzone = "..subzone)
 
 	if subzones[zone] and subzones[zone][subzone] then
 		faction = subzones[zone][subzone]
-		-- debug("setting watch on "..faction.." (subzone)")
+		-- Debug("Setting watch on "..faction.." (subzone)")
 	elseif zones[zone] then
 		faction = zones[zone]
-		-- debug("setting watch on "..faction.." (zone)")
+		-- Debug("Setting watch on "..faction.." (zone)")
 	elseif db.default then
 		faction = db.default
-		-- debug("setting watch on "..faction.." (default)")
+		-- Debug("Setting watch on "..faction.." (default)")
 	else
-		faction = racialFaction
-		-- debug("setting watch on "..faction.." (racial)")
+		faction = racial
+		-- Debug("Setting watch on "..faction.." (racial)")
 	end
 
 	if faction ~= tostring(GetWatchedFactionInfo()) then
 		self:SetWatchedFactionByName(faction)
 		if not db.verbose then
-			print("Now watching "..faction..".")
+			Print("Now watching "..faction..".")
 		end
 	end
 end
@@ -190,14 +189,15 @@ function Diplomancer:ExpandFactionHeaders()
 	local name, isHeader, isCollapsed
 	local n = GetNumFactions()
 	for i = 0, n, 1 do
-		name, _, _, _, _, _, _, _, isHeader, isCollapsed, _ = GetFactionInfo(i)
+		name, _, _, _, _, _, _, _, isHeader, isCollapsed = GetFactionInfo(i)
 		if isHeader then
 			if isCollapsed and name ~= "Inactive" then
 				ExpandFactionHeader(i)
 				n = GetNumFactions()
 				i = 0
 			elseif name == "Inactive" then
-				CollapseFactionHeader(i)
+			--	@DEV: don't collapse anymore, it's annoying if I'm trying to look at the window
+			--	CollapseFactionHeader(i)
 				break
 			end
 		end
@@ -208,56 +208,50 @@ end
 	Handle slash commands
 --------------------------------------------------------------]]
 
+local options = {
+	[L["default"]] = "SetDefaultFaction",
+	[L["verbose"]] = "ToggleVerboseMode",
+}
+
 SLASH_DIPLOMANCER1 = "/diplomancer"
 SLASH_DIPLOMANCER2 = "/dm"
 SlashCmdList.DIPLOMANCER = function(input)
-	local cmd, args = string.match(input, "^%s*(%S+)%s*(.*)$")
-	local handler = cmd and Diplomancer["CMD_"..cmd:upper()]
-	-- debug("cmd = "..cmd.."; args = "..(args or "nil").."; handler "..(handler and "found" or "not found"))
-	if handler then
-		handler(Diplomancer, args)
-	else
-		print("Automatic zone-based faction watch")
-		ChatFrame1:AddMessage("   Use |cffffff99/diplomancer|r or |cffffff99/dm|r with the following commands:")
-		ChatFrame1:AddMessage("   - |cffff9933d|r|cffffff99efault|r - set your default faction")
-		ChatFrame1:AddMessage("   - |cffff9933v|r|cffffff99erbose|r - print a message when changing factions")
-	end
+	local cmd, etc = string.match(input, "^%s*(%S+)%s*(.*)$")
+	local func = cmd and options[cmd]
+	if Diplomancer[func] then return Diplomancer[func](Diplomancer, etc) end
+	Print(L["Automatic location-based faction watching"])
+	DEFAULT_CHAT_FRAME:AddMessage(L["Use /diplomancer or /dm with the following commands:"])
+	DEFAULT_CHAT_FRAME:AddMessage("   "..L["default"].." - "..L["set your default faction"])
+	DEFAULT_CHAT_FRAME:AddMessage("   "..L["verbose"].." - "..L["print a message when changing factions"])
 end
 
-function Diplomancer:CMD_DEFAULT(input)
-	if input and input:len() > 0 then
-		if string.lower(input) == "none" then
-			print("Default faction reset to racial faction.")
+function Diplomancer:SetDefaultFaction(str)
+	if str and str:len() > 0 then
+		str = string.lower(str)
+		if str == L["none"] or str == L["reset"] then
+			Print(L["Default faction reset to racial faction."])
 			db.default = nil
 			self:Update()
 		else
-			local found, faction = self:FindFactionName(input)
+			local found, faction = self:FindFactionName(str)
 			if found then
-				print("Default faction set to \""..faction.."\".")
 				db.default = faction
+				Print(L["Default faction set to \"%s\"."], faction)
 				self:Update()
 			else
-				print("Could not find a faction matching \""..input.."\".")
+				Print(L["Could not find a faction matching \"%s\"."], str)
 			end
 		end
 	else
-		print("Default faction currently set to \""..(db.default or racialFaction).."\".")
+		Print(string.format(L["Default faction currently set to \"%s\"."], db.default or racial)
 	end
 end
-Diplomancer.CMD_D = Diplomancer.CMD_DEFAULT
 
-function Diplomancer:CMD_VERBOSE(input)
-	if input ~= nil and (input == "on" or input == "true" or input == "1") then
-		db.verbose = true
-	elseif input ~= nil and (input == "off" or input == "false" or input == "0") then
-		db.verbose = false
-	else
-		db.verbose = not db.verbose
-	end
+function Diplomancer:ToggleVerboseMode()
+	db.verbose = not db.verbose
 	if db.verbose then
-		print("Verbose mode enabled.")
+		Print(L["Verbose mode enabled."])
 	else
-		print("Verbose mode disabled.")
+		Print(L["Verbose mode disabled."])
 	end
 end
-Diplomancer.CMD_V = Diplomancer.CMD_VERBOSE
