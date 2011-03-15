861bf9bebc86c0a47e2e0d14f6ff72c003b1a7ac
Akkorian-257529 <Akkorian-257529@50dd075b-85b6-4ff6-937e-02ad84eb66a8>
2011-03-15 21:46:44 +0000

    Added Koran translations by talkswind on CurseForge Added Russian translations Updated German and French translations

--- trunk/Localization.lua
+++ trunk/Localization.lua
@@ -12,15 +12,10 @@ local LOCALE = GetLocale()
 if LOCALE:match("^en") then return end
 
 local _, Diplomancer = ...
-if not Diplomancer then -- WoW China is still running 3.2
-	Diplomancer = { }
-	_G.Diplomancer = Diplomancer
-end
 
 --[[--------------------------------------------------------------------
 	German | Deutsch
-	Contributors: paterbrown
-	Last updated: 2009-02-07
+	Last updated: 2009-02-07 by paterbrown
 ----------------------------------------------------------------------]]
 
 if LOCALE == "deDE" then Diplomancer.L = {
@@ -29,24 +24,23 @@ if LOCALE == "deDE" then Diplomancer.L = {
 	["Select a faction to watch when your current location doesn't have an associated faction."] = "Wähle eine Standardfraktion, welche angezeigt wird wenn dein Aufenthaltsort nicht mit einer Fraktion verknüpft ist.",
 
 	["Reset"] = "Reset",
-	["Reset your default faction to your race's faction."] = "Zurücksetzen der Standardfraktion auf deine Volksfraktion.",
+	["Reset your default faction preference to your race's faction."] = "Zurücksetzen der Standardfraktion auf deine Volksfraktion.",
 
---	["Default to championed faction"] = "",
---	["Use your currently championed faction as your default faction."] = "",
+	["Default to championed faction"] = "Verwenden Tragtenfraktion",
+	["Use your currently championed faction as your default faction."] = "Verwenden die Tragtenfraktion als deine Standardfraktion.",
 
 	["Ignore Exalted factions"] = "Ignoriere ehrfürchtige Fraktionen",
 	["Don't watch factions with whom you have already attained Exalted reputation."] = "Zeigt Fraktionen mit ehrfürchtigem Ruf nicht mehr an.",
 
 	["Announce watched faction"] = "Schalte Benachrichtigungen ein",
 	["Show a message in the chat frame when your watched faction is changed."] = "Zeigt eine Meldung im Chatfenster wenn sich die angezeigte Fraktion ändert.",
---	["Now watching %s."] = "",
+	["Now watching %s."] = "Angezeigte %s.",
 
 } return end
 
 --[[--------------------------------------------------------------------
 	Spanish | Español
-	Contributors: Akkorian
-	Last updated: 2011-02-28
+	Last updated:2011-02-28 by Akkorian
 ----------------------------------------------------------------------]]
 
 if LOCALE == "esES" or LOCALE == "esMX" then Diplomancer.L = {
@@ -55,7 +49,7 @@ if LOCALE == "esES" or LOCALE == "esMX" then Diplomancer.L = {
 	["Select a faction to watch when your current location doesn't have an associated faction."] = "Seleccionar una facción para seguir, mientras estás en lugares sin facciones asociadas.",
 
 	["Reset"] = "Restablecer",
-	["Reset your default faction to your race's faction."] = "Restablecer tu facción predeterminado al facción de tu raza.",
+	["Reset your default faction preference to your race's faction."] = "Restablecer tu facción predeterminado al facción de tu raza.",
 
 	["Default to championed faction"] = "Utilizar facción abanderada",
 	["Use your currently championed faction as your default faction."] = "Utilizar tu facción abanderada como tu facción predeterminado.",
@@ -63,7 +57,7 @@ if LOCALE == "esES" or LOCALE == "esMX" then Diplomancer.L = {
 	["Ignore Exalted factions"] = "Ignorar facciones Exaltados",
 	["Don't watch factions with whom you have already attained Exalted reputation."] = "No seguir las facciones con las que ya ha alcanzado el nivel de Exaltado.",
 
-	["Announce watched faction"] = "Anunciar seguido facción",
+	["Announce watched faction"] = "Anunciar facción seguido",
 	["Show a message in the chat frame when your watched faction is changed."] = "Mostrar un mensaje en la ventana de chat cuando Diplomancer cambia tu facción seguido.",
 	["Now watching %s."] = "Ahora siguiendo %s.",
 
@@ -71,8 +65,7 @@ if LOCALE == "esES" or LOCALE == "esMX" then Diplomancer.L = {
 
 --[[--------------------------------------------------------------------
 	French | Français
-	Contributors: UNKNOWN
-	Last updated: 2010-08-05
+	Last updated: 2010-08-05 by Akkorian
 ----------------------------------------------------------------------]]
 
 if LOCALE == "frFR" then Diplomancer.L = {
@@ -81,76 +74,74 @@ if LOCALE == "frFR" then Diplomancer.L = {
 	["Select a faction to watch when your current location doesn't have an associated faction."] = "Sélectionnez une faction de regarder si votre position actuelle n'est pas une faction associée.",
 
 	["Reset"] = "Remise",
-	["Reset your default faction to your race's faction."] = "Réinitialiser votre préférence faction par défaut à la faction de votre course.",
+	["Reset your default faction preference to your race's faction."] = "Réinitialiser votre préférence faction par défaut à la faction de votre course.",
 
---	["Default to championed faction"] = "",
---	["Use your currently championed faction as your default faction."] = "",
+	["Default to championed faction"] = "Par défaut à votre faction défendu",
+	["Use your currently championed faction as your default faction."] = "Utiliser votre faction défendu que votre faction par défaut.",
 
 	["Ignore Exalted factions"] = "Ignorer factions Exalté",
---	["Don't watch factions with whom you have already attained Exalted reputation."] = "",
+	["Don't watch factions with whom you have already attained Exalted reputation."] = "Ne pas regarder factions avec lesquelles vous avez déjà atteint debout Exalté.",
 
 	["Announce watched faction"] = "Activer les notifications",
 	["Show a message in the chat frame when your watched faction is changed."] = "Afficher un message dans le chat cadre lorsque vos modifications faction regardé.",
---	["Now watching %s."] = "",
+	["Now watching %s."] = "Maintenant regarder %s.",
 
 } return end
 
 --[[--------------------------------------------------------------------
 	Russian | Русский
-	Contributors: UNKNOWN
-	Last updated: YYYY-MM-DD
+	Last updated:2011-03-15 by Akkorian
 ----------------------------------------------------------------------]]
 
 if LOCALE == "ruRU" then Diplomancer.L = {
 
---	["Default faction"] = "",
---	["Select a faction to watch when your current location doesn't have an associated faction."] = "",
+	["Default faction"] = "Стандартные фракция",
+	["Select a faction to watch when your current location doesn't have an associated faction."] = "Выберите фракции следить за выполнением фракция, когда ни одна фракция связана с вашего текущего местоположения.",
 
---	["Reset"] = "",
---	["Reset your default faction to your race's faction."] = "",
+	["Reset"] = "Сброс",
+	["Reset your default faction preference to your race's faction."] = "Сброс стандартных фракции в фракцию вашей расы.",
 
---	["Default to championed faction"] = "",
---	["Use your currently championed faction as your default faction."] = "",
+	["Default to championed faction"] = "Отстаивал фракции",
+	["Use your currently championed faction as your default faction."] = "Следуйте за выполнением вашей отстаивала фракция по умолчанию.",
 
---	["Ignore Exalted factions"] = "",
---	["Don't watch factions with whom you have already attained Exalted reputation."] = "",
+	["Ignore Exalted factions"] = "Нет Превознесение фракций",
+	["Don't watch factions with whom you have already attained Exalted reputation."] = "Не следить за выполнением фракциями, с которыми вы уже достигли Превознесение стоя.",
 
---	["Announce watched faction"] = "",
---	["Show a message in the chat frame when your watched faction is changed."] = "",
---	["Now watching %s."] = "",
+	["Announce watched faction"] = "Уведомлять на изменения",
+	["Show a message in the chat frame when your watched faction is changed."] = "Уведомлять, когда вы будете следить за выполнением фракции.",
+	["Now watching %s."] = "Теперь следующие за выполнением %s.",
 
 } return end
 
 --[[--------------------------------------------------------------------
 	Korean | 한국어
-	Contributors: UNKNOWN
-	Last updated: YYYY-MM-DD
+	Last updated:2011-03-03 by talkswind @ CurseForge
 ----------------------------------------------------------------------]]
 
 if LOCALE == "koKR" then Diplomancer.L = {
 
---	["Default faction"] = "",
---	["Select a faction to watch when your current location doesn't have an associated faction."] = "",
+	["Default faction"] = "기본 진영",
+	["Select a faction to watch when your current location doesn't have an associated faction."] = "현재 위치가 관련된 진영이 없는 경우에 감시할 진영을 선택합니다.",
+	
+	["Default to championed faction"] = "옹호하고 있는 진영을 기본 진영으로",
+	["Use your currently championed faction as your default faction."] = "현재 옹호하고 있는 진영을 기본 진영으로 사용합니다.",
 
---	["Reset"] = "",
---	["Reset your default faction to your race's faction."] = "",
+	["Reset"] = "초기화",
+	["Reset your default faction preference to your race's faction."] = "기본 선호 진영을 종족의 진영으로 초기화합니다. ",
 
---	["Default to championed faction"] = "",
---	["Use your currently championed faction as your default faction."] = "",
-
---	["Ignore Exalted factions"] = "",
---	["Don't watch factions with whom you have already attained Exalted reputation."] = "",
-
---	["Announce watched faction"] = "",
---	["Show a message in the chat frame when your watched faction is changed."] = "",
---	["Now watching %s."] = "",
+	["Ignore Exalted factions"] = "확고한 동맹 진영 무시",
+	["Don't watch factions with whom you have already attained Exalted reputation."] = "확고한 동맹 평판을 이미 달성한 진영은 감시하지 않습니다. ",
+	
+	["Announce watched faction"] = "감시 중인 진영 알림",
+	["Show a message in the chat frame when your watched faction is changed."] = "감시 중인 진영이 변경된 경우에 대화창에 메시지를 보여줍니다.",
+	["Now watching %s."] = "%s|1을;를; 지금 감시 중에 있습니다.",
 
 } return end
 
 --[[--------------------------------------------------------------------
 	Simplified Chinese | 简体中文
-	Contributors: VENSTER, 急云@CWDG
-	Last updated: 2010-08-05
+	Last updated:2010-08-05 by VENSTER
+	Previous contributors: 急云@CWDG
 ----------------------------------------------------------------------]]
 
 if LOCALE == "zhCN" then Diplomancer.L = {
@@ -159,7 +150,7 @@ if LOCALE == "zhCN" then Diplomancer.L = {
 	["Select a faction to watch when your current location doesn't have an associated faction."] = "当目前所在区域没有对应声望时，选择监视的声望",
 
 	["Reset"] = "重置",
-	["Reset your default faction to your race's faction."] = "将你的缺省声望重置为种族声望",
+	["Reset your default faction preference to your race's faction."] = "将你的缺省声望重置为种族声望",
 
 --	["Default to championed faction"] = "",
 --	["Use your currently championed faction as your default faction."] = "",
@@ -175,8 +166,7 @@ if LOCALE == "zhCN" then Diplomancer.L = {
 
 --[[--------------------------------------------------------------------
 	Traditional Chinese | 正體中文
-	Contributors: 急云@CWDG
-	Last updated: 2010-01-23
+	Last updated: 2010-01-23 by 急云@CWDG
 ----------------------------------------------------------------------]]
 
 if LOCALE == "zhTW" then Diplomancer.L = {
@@ -185,7 +175,7 @@ if LOCALE == "zhTW" then Diplomancer.L = {
 	["Select a faction to watch when your current location doesn't have an associated faction."] = "當目前所在區域沒有對應聲望時，選擇監視的聲望",
 
 	["Reset"] = "重置",
-	["Reset your default faction to your race's faction."] = "將你的缺省聲望重置為種族聲望",
+	["Reset your default faction preference to your race's faction."] = "將你的缺省聲望重置為種族聲望",
 
 --	["Default to championed faction"] = "",
 --	["Use your currently championed faction as your default faction."] = "",
