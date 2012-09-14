a7840bbcc5097dc730389c0d533d8eaa5db42e4d
Phanx-28751 <Phanx-28751@50dd075b-85b6-4ff6-937e-02ad84eb66a8>
2012-09-14 11:56:33 +0000

    - Detect tabard changes outside of instances, since we can default to our championed faction now.

--- trunk/Core.lua
+++ trunk/Core.lua
@@ -83,12 +83,12 @@ function Diplomancer:PLAYER_LOGIN()
 	subzoneFactions = self.subzoneFactions
 	zoneFactions = self.zoneFactions
 
+	self.frame:RegisterEvent("PLAYER_CONTROL_GAINED")
 	self.frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+	self.frame:RegisterEvent("UNIT_INVENTORY_CHANGED")
 	self.frame:RegisterEvent("ZONE_CHANGED")
 	self.frame:RegisterEvent("ZONE_CHANGED_INDOORS")
 	self.frame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
-
-	self.frame:RegisterEvent("PLAYER_CONTROL_GAINED")
 
 	self.frame:UnregisterEvent("PLAYER_LOGIN")
 	self.PLAYER_LOGIN = nil
@@ -213,22 +213,9 @@ end
 
 ------------------------------------------------------------------------
 
-function Diplomancer:PLAYER_ENTERING_WORLD()
-	-- self:Debug("PLAYER_ENTERING_WORLD")
-	local _, instanceType = IsInInstance()
-	if instanceType == "party" then
-		self.frame:RegisterEvent("UNIT_INVENTORY_CHANGED")
-	else
-		self.frame:UnregisterEvent("UNIT_INVENTORY_CHANGED")
-	end
-	self:Update()
-end
-
-------------------------------------------------------------------------
-
-function Diplomancer:UNIT_INVENTORY_CHANGED(unit)
+function Diplomancer:UNIT_INVENTORY_CHANGED(_, unit)
 	if unit == "player" then
-		-- self:Debug("UNIT_INVENTORY_CHANGED")
+		self:Debug("UNIT_INVENTORY_CHANGED")
 		self:Update()
 	end
 end
